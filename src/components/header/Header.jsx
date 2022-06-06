f8eb1b79fc69db69cbfc3cf67f402df8f6027730
Lalit <lalitkumarsharma.jsr@gmail.com>
2022-06-06 20:12:59 +0530

    added header & nav components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import './header.css'
+import "./header.css"
+import CTA from './CTA'
+import ME from '../../assets/ME.png'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className=".container.header__container">
+        <h5>Hello I'm</h5>
+        <h1>Lalit Kumar Sharma</h1>
+        <h5 className='text-light'>Software Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+         <img src={ME} alt='me' class='.me' />
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+
+    </header>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {GoHome} from 'react-icons/go'
+import {BiUserCircle} from 'react-icons/bi'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {RiMessageLine} from 'react-icons/ri'
+import {useState} from 'react'
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#"?'active':''}><GoHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')}className={activeNav === '#about'?'active':''}><BiUserCircle /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')}className={activeNav === '#experience'?'active':''}><BiBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')}className={activeNav === '#services'?'active':''}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')}className={activeNav === '#contact'?'active':''}><RiMessageLine /></a>
+    </nav>
   )
 }
 
