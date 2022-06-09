3d92ca0722c316af7c8e690e837b1e1ff04430d8
Lalit <lalitkumarsharma.jsr@gmail.com>
2022-06-09 18:19:42 +0530

    Added Services and Portfolio components

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
 import './portfolio.css'
+import {SiGithub} from 'react-icons/si'
+import {MdComputer} from 'react-icons/md'
+import IMG1 from '../../assets/bird.jpg'
+
+const data = [
+    {
+      id:1,
+      image:IMG1,
+      title: 'Human Pose Detector-OpenCV Project',
+      github: 'https://github.com/lalitkrsharma/Human-Pose-Detector-Open-CV-.git',
+      demo: 'https://github.com/lalitkrsharma/Human-Pose-Detector-Open-CV-.git'
+    },
+
+    {
+      id:2,
+      image:IMG1,
+      title: 'Human Pose Detector-OpenCV Project',
+      github: 'https://github.com/lalitkrsharma/Human-Pose-Detector-Open-CV-.git',
+      demo: 'https://github.com/lalitkrsharma/Human-Pose-Detector-Open-CV-.git'
+    }
+]
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+       
+        {
+          data.map(({id ,image, title, github ,demo}) => {
+            return(
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>
+                    <div>
+                      <SiGithub className='.portfolio__item-icon'/>
+                    </div>
+                    Github
+                  </a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>
+                    <div>
+                      <MdComputer className='.portfolio__item-icon'/>
+                    </div>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
 import './services.css'
+import {BsCheck2All} from 'react-icons/bs'
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>  
+
+      <div className='container services__container'>
+        <article className='services'>
+          <div className='services__head'>
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='services__list'>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End Of Web Dev ==========================================*/}
+
+        <article className='services'>
+          <div className='services__head'>
+            <h3>Software Development</h3>
+          </div>
+
+          <ul className='services__list'>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+            <li>
+              <BsCheck2All className='services__list-icon' />
+              <p>Made this very portfolio site using Javascript.</p>
+            </li>
+          </ul>
+        </article>
+    {/* End Of Software Dev ===================================================*/}
+      </div>
+
+    </section>
   )
 }
 
